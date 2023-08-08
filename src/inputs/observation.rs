7944247965f8bcad59a22dd03ba32339a36267b9
JamesMcGuigan <james.mcguigan@gmail.com>
2023-08-08 01:15:53 +0100

    Observation::from_array(board: ObservationArray)

--- src/inputs/observation.rs
+++ src/inputs/observation.rs
@@ -6,7 +6,7 @@ pub type ObservationArray = [PlayerID; (MAX_COLS * MAX_ROWS) as usize];  // == [
 
 // DOCS: https://pyo3.rs/v0.13.2/class.html
 #[pyclass]
-#[derive(Clone)]
+#[derive(Debug, Clone, PartialEq)]
 #[allow(non_snake_case, dead_code)]
 pub struct Observation {
     #[pyo3(get)] pub step: u8,
@@ -18,11 +18,27 @@ pub struct Observation {
 #[allow(non_snake_case, dead_code)]
 impl Observation {
     #[new]
-    fn new(step: u8, mark: u8, board: [u8;42], remainingOverageTime: f32) -> Self {
+    fn new(step: u8, mark: u8, board: ObservationArray, remainingOverageTime: f32) -> Self {
         Observation { step, mark, board, remainingOverageTime }
     }
 }
 
+#[allow(dead_code)]
+impl Observation {
+    fn from_array(board: ObservationArray) -> Self {
+        let step = board.iter()
+            .filter(|&square| { *square != 0 as PlayerID })
+            .count() as u8;
+        let mark = if step % 2 == 0 { 1 } else { 2 };
+
+        Observation {
+            step,
+            mark,
+            board,
+            remainingOverageTime: 0.0,
+        }
+    }
+}
 
 /// obs  = { 'remainingOverageTime': 60, 'step': 0, 'mark': 1, 'board': [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
 /// conf = { 'timeout': 2, 'actTimeout': 2, 'agentTimeout': 60, 'episodeSteps': 1000, 'runTimeout': 1200, 'columns': 7, 'rows': 6, 'inarow': 4, '__raw_path__': '/kaggle_simulations/agent/main.py' }
@@ -32,7 +48,42 @@ impl Default for Observation {
             step: 0,
             mark: 1,
             board: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            remainingOverageTime: 60.0,
+            remainingOverageTime: 0.0,
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod from_array {
+        use super::*;
+
+        #[test]
+        fn test_default() {
+            let input  = Observation::default();
+            let output = Observation::from_array(input.board);
+            assert_eq!(input, output);
+        }
+
+        #[test]
+        fn test_first_move() {
+            let input = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1];
+            let output = Observation::from_array(input);
+            assert_eq!(output.board, input);
+            assert_eq!(output.step,  1);
+            assert_eq!(output.mark,  2);
+        }
+
+        #[test]
+        fn test_second_move() {
+            let input = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,1];
+            let output = Observation::from_array(input);
+            assert_eq!(output.board, input);
+            assert_eq!(output.step,  2);
+            assert_eq!(output.mark,  1);
         }
     }
 }
