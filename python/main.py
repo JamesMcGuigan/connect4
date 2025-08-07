b495d5e8284567e54c5796e3aecc7b868b28831f
JamesMcGuigan <james.mcguigan@gmail.com>
2025-08-07 20:52:17 +0100

    BUGFIX: TypeError: Configuration.__new__() got an unexpected keyword argument 'seed'\n`

--- python/main.py
+++ python/main.py
@@ -15,7 +15,10 @@ from kaggle_environments import structify
 
 ### Kaggle Agent Function
 def kaggle_agent(obs, conf):
+    print('Observation', obs)
+    print('Configuration', conf)
     if '__raw_path__' in conf: del conf['__raw_path__']  # __raw_path__ removed from Observation.rs
+    if 'seed'         in conf: del conf['seed']          # TypeError: Configuration.__new__() got an unexpected keyword argument 'seed'\n`
     action = submission_agent(obs, conf)  # agent.py
     return action
 
