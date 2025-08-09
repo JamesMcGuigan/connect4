66c7455942ac59ddc02dc13633b3911c587e4b63
JamesMcGuigan <james.mcguigan@gmail.com>
2025-08-09 16:18:28 +0100

    agent_greedy.rs | refactor

--- src/agents/agent_greedy.rs
+++ src/agents/agent_greedy.rs
@@ -9,26 +9,30 @@ pub fn agent_greedy(obs: Observation, _conf: Configuration) -> u8 {
     let board: BoardBitmask = obs.into(); // Convert Observation to BoardBitmask
     let player_id = <BoardBitmask as Board>::get_move_player(&board);
 
-    // // Check for winning move
-    // for col in board.get_valid_actions() {
-    //     if let Some(next_board) = board.step(col) {
-    //         if next_board.is_win(player_id) {
-    //             return col;
-    //         }
-    //     }
-    // }
+    // Default to column 3 (center) if first move
+    if obs.step == 0 {
+        return 3;
+    }
+
+    // Check for winning move
+    for col in board.get_valid_actions() {
+        if let Some(next_board) = board.step(col) {
+            if next_board.is_win(player_id) {
+                return col;
+            }
+        }
+    }
 
     // Greedy pick huristic score for depth=1
-    let chosen_col = board
-        .get_valid_actions()
-        .into_iter()
-        .filter_map(|col|
-            board.step(col)
-                .map(|b| (col, b.huristic_score(player_id)))
-        )
-        .max_by_key(|&(_, score)| score)
-        .map(|(col, _)| col)
-        .unwrap_or(3);  // Default to column 3 (center) if no valid actions
-
-    return chosen_col;
+    let mut max_score = 0;
+    let mut max_col   = 3;
+    for col in board.get_valid_actions() {
+        let state = board.step(col);
+        let score = state.unwrap().huristic_score(player_id);
+        if score > max_score {
+            max_score = score;
+            max_col   = col;
+        }
+    }
+    return max_col;
 }
